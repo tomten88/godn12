ef400e00fc3647097c923337c3c8474d408e9063
tomten88 <103364853+tomten88@users.noreply.github.com>
2022-06-05 18:48:43 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -68,7 +68,7 @@ For More Details Click Help Button Below..
     
     ABOUT_TEXT = """<b>➥ Name</b> : <code> Auto Filter Bot</code>
     
-<b>➥ Creator</b> : <b><i><a href="https://@mr_tom_kiddo">AlbertEinstein_TG</a></i></b>
+<b>➥ Creator</b> : <b><i><a href="https://@mr_tom_kiddo">@mr_tom_kiddo</a></i></b>
 
 <b>➥ Language</b> : <code>Python3</code>
 
